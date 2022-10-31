95ac5657a8b73e42a1f8f5675a9268dde19e0bb3
jamiegledhill <32931760+jamiegledhill@users.noreply.github.com>
2022-10-31 20:12:08 +0000

    Add files via upload

---
+++ js/js-scroll.js
@@ -0,0 +1,22 @@
+//UTILITY
+// throttle - fn = function to call, wait = interval in ms
+function throttle(fn, wait){
+  let inThrottle, lastFn, lastTime;
+  return function() {
+    const context = this,
+      args = arguments;
+    if (!inThrottle) {
+      fn.apply(context, args);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(function() {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(context, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
+  };
+};
